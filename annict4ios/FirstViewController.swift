06de7c01724f9419559b27f9d05a55c0cb74d1f9
alitaso345 <a.maruyama@uec.ac.jp>
2017-04-04 14:08:48 +0900

    Initial Commit

---
+++ annict4ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  annict4ios
+//
+//  Created by arisu-maruyama on 2017/04/04.
+//  Copyright © 2017年 alitaso345. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ annict4ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  annict4ios
+//
+//  Created by arisu-maruyama on 2017/04/04.
+//  Copyright © 2017年 alitaso345. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
